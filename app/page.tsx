c756aafa33446a79f6f600acfa7c0a8134cd2bf3
Imalsha Sathsarani <158012123+ImalshaSathsarani@users.noreply.github.com>
2025-11-28 18:39:18 +0530

    Fix a depolyment error

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
 import { IEvent } from "@/database";
-import events from "@/lib/constants"
+//import events from "@/lib/constants"
 import { cacheLife } from "next/cache";
+import events, { EventItem } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async  () => {
   'use cache';
   cacheLife('hours')
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await response.json();
 
 
   return (
@@ -24,7 +25,7 @@ const Page = async  () => {
       <h3>Featured Events</h3>
 
       <ul className="events">
-        {events && events.length >0 && events.map((event:IEvent)=>(
+        {events && events.length >0 && events.map((event:EventItem)=>(
           <li key={event.title} className="list-none">
             <EventCard { ...event} />
           </li>
